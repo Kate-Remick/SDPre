72bdd66a48341fa33c623260f2a5d216ac57afbd
Katherine <remick.ka@gmail.com>
2022-02-23 18:32:42 -0800

    complete objective2lab2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,9 @@
+public class Objective2Lab2 {
+  public static void main(String[] args) {
+    String name = "Katherine Remick";
+    int age = 26;
+    String state = "Kansas";
+    System.out.println("Name: " + name + " Age: " + age + " State: " + state);
+
+  }
+}
