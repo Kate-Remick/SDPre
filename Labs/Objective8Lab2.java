4091b831bb45052a140e95be509bfa9bbb8ddbc0
Katherine <remick.ka@gmail.com>
2022-02-24 14:28:48 -0800

    completed objective8lab2

---
+++ Labs/Objective8Lab2.java
@@ -0,0 +1,12 @@
+public class Objective8Lab2 {
+  public static void main(String[] args) {
+  int sum = 0;
+  int count = 1;
+
+
+  for(count = 1; count <= 20; count ++) {
+    sum = sum + (count);
+    }
+    System.out.println(sum);
+  }
+}
