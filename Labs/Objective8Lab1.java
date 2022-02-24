5956fd0941af69c543aa7b5d439ab76e199d4b4a
Katherine <remick.ka@gmail.com>
2022-02-24 14:22:14 -0800

    Completed Objective8Lab1

---
+++ Labs/Objective8Lab1.java
@@ -0,0 +1,13 @@
+public class Objective8Lab1 {
+  public static void main(String[] args) {
+    int count1 = 1;
+    int count2 = 10;
+
+    for(count1 = 1; count1 <=10; count1 ++) {
+      System.out.println(count1);
+    }
+    for(count2 = 10; count2 >= 1; count2 --) {
+      System.out.println(count2);
+    }
+  }
+}
